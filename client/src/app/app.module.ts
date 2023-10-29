8e8c305f9792b26ba5b443f369e5c331a6af19aa
kifle23 <kiflekifle23@gmail.com>
2023-10-29 18:08:28 +0300

    Create required components and configure route

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    JoinRoomComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
