07e6bfecc2f268ad39844533890ed04ff498b019
kifle23 <kiflekifle23@gmail.com>
2023-10-30 21:21:02 +0300

    Create form group for join room

--- client/src/app/join-room/join-room.component.ts
+++ client/src/app/join-room/join-room.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join-room',
   templateUrl: './join-room.component.html',
-  styleUrls: ['./join-room.component.css']
+  styleUrls: ['./join-room.component.css'],
 })
-export class JoinRoomComponent {
+export class JoinRoomComponent implements OnInit {
+  joinRoomForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
+    this.joinRoomForm = this.fb.group({
+      user: ['', Validators.required],
+      room: ['', Validators.required],
+    });
+  }
+
+  joinRoom(): void {
+    this.router.navigate(['/chat'], {
+      queryParams: this.joinRoomForm.value,
+    });
+  }
 }
