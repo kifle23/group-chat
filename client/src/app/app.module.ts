3a5bc8da65539d15b4db74dcc7e31fbd2c311583
kifle23 <kiflekifle23@gmail.com>
2023-11-02 20:30:58 +0300

    Add routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { JoinRoomComponent } from './join-room/join-room.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'join-room', component: JoinRoomComponent },
+  { path: 'chat', component: ChatComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     JoinRoomComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
